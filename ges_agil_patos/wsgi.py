89979f2d86de5e37166381b4c392b06d93138055
Nicolás Andrews <nm.andrewss@gmail.com>
2021-05-28 19:45:31 -0400

    start django project

---
+++ ges_agil_patos/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ges_agil_patos project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ges_agil_patos.settings')
+
+application = get_wsgi_application()
---
+++ readme.md
@@ -0,0 +1,12 @@
+## Proyecto Gestión Ágil - 🔥 鴨神 🦆 
+
+Proyecto semestral de la asginatura gestión agil UTEM 2021-1
+
+Aplicación web realizada en django que permite a usuarios agendar horas de atención para _nombre\_empresa_ y otras cosas.
+
+#### Autores
+- Daniel Aguilera
+- Nicolás Andrews
+- Rodrigo Carmona
+- Humberto Roman
+
