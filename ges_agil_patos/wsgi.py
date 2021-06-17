b0f4f5a7aa2f50e6fd5c5739124262c7bf8017e1
Nicolás Andrews <nm.andrewss@gmail.com>
2021-06-16 20:22:10 -0400

    blackenize code

--- ges_agil_patos/wsgi.py
+++ ges_agil_patos/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ges_agil_patos.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ges_agil_patos.settings")
 
 application = get_wsgi_application()
