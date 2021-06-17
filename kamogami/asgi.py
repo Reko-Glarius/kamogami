ea4f700bf87d7d073cbd756266baf72b8037ac0c
Nicolás Andrews <nm.andrewss@gmail.com>
2021-06-16 20:30:16 -0400

    rename app to kamogami

--- kamogami/asgi.py
+++ kamogami/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ges_agil_patos project.
+ASGI config for kamogami project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ges_agil_patos.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kamogami.settings")
 
 application = get_asgi_application()
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ges_agil_patos.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kamogami.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
