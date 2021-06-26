587cfc82b815c1b16382d763ecb2d20706d8459d
Nicolás Andrews <nm.andrewss@gmail.com>
2021-06-26 17:24:04 -0400

    add django models

--- calendario/models.py
+++ calendario/models.py
@@ -1,4 +1,8 @@
 from django.db import models
 
-# class calendario(models.Model):
-    
+class attention_hours(models.Model):
+    day = models.DateField()
+    start_time = models.TimeField(null=True, default=None)
+    end_time = models.TimeField(null=True, default=None)
+    valid_from = models.DateField(null=True, default=None)
+    valid_until = models.DateField(default=None, null=True)
---
+++ reservas/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+from kamogami.constants import STATUS_CHOICES
+
+class servicio(models.Model):
+    time_slots = models.IntegerField() # 1 time slot = 30 mins
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    reference_price = models.IntegerField()
+
+    def __str_(self):
+        return f"{self.name} - {self.time_slots * 30} min"
+
+
+class reserva(models.Model):
+    servicio = models.OneToOneField(
+         servicio,
+         on_delete=models.DO_NOTHING,
+         db_constraint=False
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+    start_hour = models.TimeField()
+    end_hour = models.TimeField()
+    date = models.DateField()
+    status = models.CharField(max_length=15, choices=STATUS_CHOICES)
