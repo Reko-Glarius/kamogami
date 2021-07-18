ace2d591a82f616812bb539108366b4ccf1c84ec
Nicolás Andrews <nm.andrewss@gmail.com>
2021-07-18 18:42:01 -0400

    cambiar 'day' en 'attention_hours' por dias de la semana

--- calendario/models.py
+++ calendario/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
-
+days_name=[
+    ('L', 'Lunes'),
+    ('M', 'Martes'),
+    ('X', 'Miercoles'),
+    ('J', 'Jueves'),
+    ('V', 'Viernes'),
+    ('S', 'Sabado'),
+    ('D', 'Domingo'),
+]
 class attention_hours(models.Model):
-    day = models.DateField()
+
+    day = models.CharField(max_length=2,choices=days_name)
     start_time = models.TimeField(null=True, default=None)
     end_time = models.TimeField(null=True, default=None)
     valid_from = models.DateField(null=True, default=None)
